13865d384e8ced235007425dc634e8d1cab2ea9b
PaulGuo <badkaikai@gmail.com>
2014-09-09 11:40:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+gulp-combo
+=========
+
+A gulp plugin [gulp-combo](https://github.com/PaulGuo/gulp-combo) will combo all scripts and links in a html page.
+
+You can optionally pass a first argument along with the baseUri to override the default baseUri statements. The default baseUri looks like this:
+
+```
+baseUri = 'http://mc.yourdomainname.net/combo?f=';
+```
+
+You can also optionally pass a second argument along with the options to override the default options statements. The default options looks like this:
+
+```
+{
+	splitter: ';'
+}
+```
+
+### HTML
+
+    <!DOCTYPE HTML>
+    <html style="font-size:312.5%">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gulp-combo</title>
+        <link href="http://mc.yourdomainname.net/touch/css/awesome.0c7f2f70.css" rel="stylesheet"/>
+        <link href="http://mc.yourdomainname.net/touch/css/test.css" rel="stylesheet"/>
+    </head>
+    <body>
+    <script type="text/javascript" src="http://mc.yourdomainname.net/zepto/zepto-1.0.1.min.js"></script>
+    <script type="text/javascript" src="http://mc.yourdomainname.net/assets/js/vue.js"></script>
+    <script type="text/javascript">
+        init();
+    </script>
+
+    </body>
+    </html>
+
+### Usage
+
+    var gulp = require('gulp'),
+        combo = require('gulp-combo');
+
+    gulp.task('combo', function(){
+      var baseUri = 'http://mc.yourdomainname.net/combo?f=';
+
+      gulp.src('index.html')
+        .pipe(combo(baseUri, null))
+        .pipe(gulp.dest('build'));
+    });
+
+### Output
+
+    <!DOCTYPE HTML>
+    <html style="font-size:312.5%">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gulp-combo</title>
+        <link rel="stylesheet" href="http://mc.yourdomainname.net/combo?f=touch/css/awesome.0c7f2f70.css;touch/css/test.css" />
+    </head>
+    <body>
+    <script type="text/javascript" src="http://mc.yourdomainname.net/combo?f=zepto/zepto-1.0.1.min.js;assets/js/vue.js"></script>
+    <script type="text/javascript">
+        init();
+    </script>
+
+    </body>
+    </html>
+
+License
+----
+
+MIT
+    
---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+var through = require('through2'),
+    cheerio = require("cheerio");
+
+module.exports = function(baseUri, options) {
+  baseUri = baseUri || 'http://mc.yourdomainname.net/combo?f=';
+  options = options || {};
+
+  return through.obj(function(file, enc, cb) {
+      var $ = cheerio.load(String(file.contents));
+      var scripts, links, results;
+      var filteredScripts = [], filteredLinks = [];
+      var src = {
+          scripts: [],
+          links: []
+      };
+
+      var genComboScriptUriTag = function() {
+          var uri = baseUri + src.scripts.join(options.splitter || ';');
+          var scriptTag = '<script type="text/javascript" src="' + uri + '"></script>';
+          return scriptTag;
+      };
+
+      var genComboLinkUriTag = function() {
+          var uri = baseUri + src.links.join(options.splitter || ';');
+          var linkTag = '<link rel="stylesheet" href="' + uri + '" />';
+          return linkTag;
+      };
+
+      scripts = $('script[src]');
+      links = $('link[rel="stylesheet"][href]');
+
+      scripts.each(function(i, script) {
+          var src_attr = $(script).attr('src');
+
+          if(src_attr.match(/^http:\/\//igm)) {
+              if(src_attr.match(/^http:\/\/mc.yourdomainname.net\//igm)) {
+                filteredScripts.push(script);
+              }
+          } else {
+                filteredScripts.push(script);
+          }
+      });
+
+      links.each(function(i, link) {
+          var href_attr = $(link).attr('href');
+
+          if(href_attr.match(/^http:\/\//igm)) {
+              if(href_attr.match(/^http:\/\/mc.yourdomainname.net\//igm)) {
+                filteredLinks.push(link);
+              }
+          } else {
+                filteredLinks.push(link);
+          }
+      });
+
+      filteredScripts.forEach(function(script, i) {
+          var src_attr = $(script).attr('src');
+          
+          src.scripts.push(src_attr.replace('http://mc.yourdomainname.net/', ''));
+
+          if(i === scripts.length - 1) {
+              $(script).replaceWith('<!--SCRIPT PLACEHOLDER-->');
+          } else {
+              $(script).remove();
+          }
+      });
+
+      filteredLinks.forEach(function(link, i) {
+          var href_attr = $(link).attr('href');
+          
+          src.links.push(href_attr.replace('http://mc.yourdomainname.net/', ''));
+
+          if(i === links.length - 1) {
+              $(link).replaceWith('<!--LINK PLACEHOLDER-->');
+          } else {
+              $(link).remove();
+          }
+      });
+
+      results = $.html(null, {decodeEntities: false}).replace('<!--SCRIPT PLACEHOLDER-->', genComboScriptUriTag())
+                        .replace('<!--LINK PLACEHOLDER-->', genComboLinkUriTag());
+
+      file.contents = new Buffer(results);
+      cb(null, file);
+  });
+};
